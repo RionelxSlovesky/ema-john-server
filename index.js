ef64760c320c1b28a7c6a38ad8278a8a8a6db9ec
Ahmed Omar Salim Adnan <44505932+RionelxSlovesky@users.noreply.github.com>
2023-05-29 18:55:08 +0600

    Implemented pagination
    Loading products according to the page number, and also a specific product by its id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -33,15 +33,32 @@ async function run() {
     const productCollection = client.db('emaJohnDB').collection('products');
 
     app.get('/products', async(req, res) => {
-        const result = await productCollection.find().toArray()
+        console.log(req.query)
+
+        const page = parseInt(req.query.page) || 1
+        const limit = parseInt(req.query.limit) || 10
+
+        const skip = (page-1) * limit
+
+        const result = await productCollection.find().skip(skip).limit(limit).toArray()
         res.send(result)
     })
 
 
+    app.get("/totalProducts", async (req,res) => {
+      const result = await productCollection.estimatedDocumentCount();
+      res.send({totalProducts: result})
+    })
 
 
+    app.post('/productsByIds', async(req,res) => {
+      const ids = req.body;
+      const objectIds = ids.map(id => new ObjectId(id))
 
-
+      const products = await productCollection.find({ _id: {$in: objectIds}}).toArray();
+      res.send(products)
+      console.log(ids)
+    })
 
 
 
